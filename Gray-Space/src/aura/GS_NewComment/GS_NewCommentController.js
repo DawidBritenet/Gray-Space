17f887931aa83ca9c2cf4158c834568eaa2a55dc
unknown <dawid.surma@britenet.com.pl>
2021-06-11 08:09:59 +0200

    'community-record-view'

---
+++ Gray-Space/src/aura/GS_NewComment/GS_NewCommentController.js
@@ -0,0 +1,9 @@
+({
+    rate: function (component, event, helper) {
+        component.set('v.rate', event.currentTarget.dataset.value);
+    },
+
+    sendComment: function (component, event, helper) {
+
+    },
+});
